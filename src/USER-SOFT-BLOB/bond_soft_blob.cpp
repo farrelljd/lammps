2feed7c86525eb08602c2f6b8d8227b2f18fa1b3
James Daniel Farrell <j.d.farrell00@gmail.com>
2017-03-04 18:33:31 +0800

    fixed a bug with displaced wall--particle bonds (not sure how)

--- src/USER-SOFT-BLOB/bond_soft_blob.cpp
+++ src/USER-SOFT-BLOB/bond_soft_blob.cpp
@@ -64,33 +64,53 @@ void BondSoftBlob::get_displacement(int i1, int i2, int type, double &delx, doub
     case BLOB_WALL: {
       int i1g = tag[i1];
       int i2g = tag[i2];
- 
+
       delx = x[i1][0] - x[i2][0] - gp[i1g][i2g][0];
       dely = x[i1][1] - x[i2][1] - gp[i1g][i2g][1];
       delz = x[i1][2] - x[i2][2] - gp[i1g][i2g][2];
-      //fprintf(screen, "dx,dy,dz:\t%10.2f%10.2f%10.2f\n", delx,dely,delz);    
-      //fprintf(screen, "dx,dy,dz:\t%10.2f%10.2f%10.2f\n", x[i2][0],x[i2][1],x[i2][2]);
+      fprintf(screen, "initial dx,dy,dz:\t%10.2f%10.2f%10.2f\n", delx,dely,delz);
+      fprintf(screen, "initial dx,dy,dz:\t%10.2f%10.2f%10.2f\n", x[i2][0],x[i2][1],x[i2][2]);
 
       int *sametag = atom->sametag;
       double dx, dy, dz, rsq, rsqmin;
       rsqmin = delx*delx + dely*dely + delz*delz;
-      while (sametag[i1] >=0) {
-        i1 = sametag[i1];
-        dx = x[i1][0] - x[i2][0] - gp[i1g][i2g][0];
-        dy = x[i1][1] - x[i2][1] - gp[i1g][i2g][1];
-        dz = x[i1][2] - x[i2][2] - gp[i1g][i2g][2];
-        //fprintf(screen, "dx,dy,dz:\t%10.2f%10.2f%10.2f\n", dx,dy,dz);    
-        //fprintf(screen, "dx,dy,dz:\t%10.2f%10.2f%10.2f\n", x[i2][0],x[i2][1],x[i2][2]);
-        rsq = dx*dx + dy*dy + dz*dz;
-        if (rsq < rsqmin) {
-          rsqmin = rsq;
-          delx = dx;
-          dely = dy;
-          delz = dz;
+
+      /** I have no idea how this works **/
+      if (sametag[i2]<0) {
+        while (sametag[i1] >=0) {
+          i1 = sametag[i1];
+          dx = x[i1][0] - x[i2][0] - gp[i1g][i2g][0];
+          dy = x[i1][1] - x[i2][1] - gp[i1g][i2g][1];
+          dz = x[i1][2] - x[i2][2] - gp[i1g][i2g][2];
+          fprintf(screen, "dx,dy,dz:\t%10.2f%10.2f%10.2f\n", dx,dy,dz);
+          fprintf(screen, "dx,dy,dz:\t%10.2f%10.2f%10.2f\n", x[i2][0],x[i2][1],x[i2][2]);
+          rsq = dx*dx + dy*dy + dz*dz;
+          if (rsq < rsqmin) {
+            rsqmin = rsq;
+            delx = dx;
+            dely = dy;
+            delz = dz;
+          }
+        }
+      } else {
+        while (sametag[i2] >=0) {
+          i2 = sametag[i2];
+          dx = x[i1][0] - x[i2][0] - gp[i1g][i2g][0];
+          dy = x[i1][1] - x[i2][1] - gp[i1g][i2g][1];
+          dz = x[i1][2] - x[i2][2] - gp[i1g][i2g][2];
+          fprintf(screen, "dx,dy,dz:\t%10.2f%10.2f%10.2f\n", dx,dy,dz);
+          fprintf(screen, "dx,dy,dz:\t%10.2f%10.2f%10.2f\n", x[i2][0],x[i2][1],x[i2][2]);
+          rsq = dx*dx + dy*dy + dz*dz;
+          if (rsq < rsqmin) {
+            rsqmin = rsq;
+            delx = dx;
+            dely = dy;
+            delz = dz;
+          }
         }
       }
 
-      //fprintf(screen, "distance:\t%10.5f%10.5f%10.5f\n", delx*delx, dely*dely, delz*delz); 
+      fprintf(screen, "distance:\t%10.5f%10.5f%10.5f\n", delx*delx, dely*dely, delz*delz);
       break;
     }
     default: {
