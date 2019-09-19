a853061fcff0427e77aabb359824845e640006b3
James Daniel Farrell <j.d.farrell00@gmail.com>
2019-09-19 14:39:05 +0200

    added pair lj/cut/adress, child class of lj/cut, respa disabled

---
+++ src/USER-ADRESS/pair_lj_cut_adress.cpp
@@ -0,0 +1,136 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author: James Daniel Farrell (IOPCAS)
+------------------------------------------------------------------------- */
+
+#include "pair_lj_cut_adress.h"
+#include "atom.h"
+#include "force.h"
+#include "neigh_list.h"
+#include "error.h"
+
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+PairLJCutAdress::PairLJCutAdress(LAMMPS *lmp) : PairLJCut(lmp)
+{
+  respa_enable = 0;
+  error->all(FLERR,"pair/lj/cut adress is not ready yet :-("); 
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairLJCutAdress::compute(int eflag, int vflag)
+{
+  int i,j,ii,jj,inum,jnum,itype,jtype;
+  double xtmp,ytmp,ztmp,delx,dely,delz,evdwl,fpair;
+  double rsq,r2inv,r6inv,forcelj,factor_lj;
+  int *ilist,*jlist,*numneigh,**firstneigh;
+
+  evdwl = 0.0;
+  ev_init(eflag,vflag);
+
+  double **x = atom->x;
+  double **f = atom->f;
+  int *type = atom->type;
+  int *res = atom->res;
+  double **adw = atom->adw;
+  double wi, wj;
+  int nlocal = atom->nlocal;
+  double *special_lj = force->special_lj;
+  int newton_pair = force->newton_pair;
+
+  inum = list->inum;
+  ilist = list->ilist;
+  numneigh = list->numneigh;
+  firstneigh = list->firstneigh;
+
+  // loop over neighbors of my atoms
+
+  for (ii = 0; ii < inum; ii++) {
+    i = ilist[ii];
+    xtmp = x[i][0];
+    ytmp = x[i][1];
+    ztmp = x[i][2];
+    itype = type[i];
+    wi = adw[i][3];
+    if (wi == 0.0) continue;
+    jlist = firstneigh[i];
+    jnum = numneigh[i];
+
+    for (jj = 0; jj < jnum; jj++) {
+      j = jlist[jj];
+      factor_lj = special_lj[sbmask(j)];
+      j &= NEIGHMASK;
+
+      delx = xtmp - x[j][0];
+      dely = ytmp - x[j][1];
+      delz = ztmp - x[j][2];
+      rsq = delx*delx + dely*dely + delz*delz;
+      jtype = type[j];
+      wj = adw[j][3];
+      if (wj == 0.0) continue;
+
+      if (rsq < cutsq[itype][jtype]) {
+        r2inv = 1.0/rsq;
+        r6inv = r2inv*r2inv*r2inv;
+        forcelj = r6inv * (lj1[itype][jtype]*r6inv - lj2[itype][jtype]);
+        fpair = factor_lj*forcelj*r2inv;
+        fpair *= wi*wj;
+
+        f[i][0] += delx*fpair;
+        f[i][1] += dely*fpair;
+        f[i][2] += delz*fpair;
+        if (newton_pair || j < nlocal) {
+          f[j][0] -= delx*fpair;
+          f[j][1] -= dely*fpair;
+          f[j][2] -= delz*fpair;
+        }
+
+        if (eflag) {
+          evdwl = r6inv*(lj3[itype][jtype]*r6inv-lj4[itype][jtype]) -
+            offset[itype][jtype];
+          evdwl *= factor_lj;
+        }
+
+        if (evflag) ev_tally(i,j,nlocal,newton_pair,
+                             evdwl,0.0,fpair,delx,dely,delz);
+      }
+    }
+  }
+
+  if (vflag_fdotr) virial_fdotr_compute();
+}
+
+/* ---------------------------------------------------------------------- */
+
+double PairLJCutAdress::single(int i, int j, int itype, int jtype, double rsq,
+                         double /*factor_coul*/, double factor_lj,
+                         double &fforce)
+{
+  double r2inv,r6inv,forcelj,philj;
+  double **adw = atom->adw;
+  int *res = atom->res;
+
+  r2inv = 1.0/rsq;
+  r6inv = r2inv*r2inv*r2inv;
+  forcelj = r6inv * (lj1[itype][jtype]*r6inv - lj2[itype][jtype]);
+  fforce = adw[i][3]*adw[j][3]*factor_lj*forcelj*r2inv;
+
+  philj = r6inv*(lj3[itype][jtype]*r6inv-lj4[itype][jtype]) -
+    offset[itype][jtype];
+  return factor_lj*philj;
+}
